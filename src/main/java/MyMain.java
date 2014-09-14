63776a973764dfb55f682affe87a459cfe92d4a4
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-09-14 14:39:57 -0700

    Actually using native library works!

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,6 +2,6 @@
 public class MyMain {
     public static void main(String[] args) {
         System.out.println("MyMain: Hello, World!");//<<<
-        System.out.println(JTNoodle.class);
+        System.out.println(JTNoodle.class + " " + JTNoodle.summer());
     }
 }
